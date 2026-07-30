af1ffe5a35186fb4afc2db5622dd6c00c924ab33
aries510 <darenrespicio@gmail.com>
2026-07-29 20:41:30 -0600

    started working on adjacent cells function

--- client/Aries-Minesweeper/src/utils/getAdjacentCells.js
+++ client/Aries-Minesweeper/src/utils/getAdjacentCells.js
@@ -1,5 +1,27 @@
 export function getAdjacentCells(board, row, column) {
-    const adjacentCells = [];
-    row = 10;
-    
+    const neighbors = [];
+    const rows = 10;
+    const columns = 10;
+    const possibleNeighborPositions = 
+    [
+        [-1, -1],
+        [-1, 0],
+        [-1, +1],
+        [0, -1],
+        [0, +1],
+        [+1, -1],
+        [+1, 0],
+        [+1, +1]
+
+    ];
+
+    for(let i = 0; i < possibleNeighborPositions.length; i++) {
+        for(let j = 0; j < rows; j++) {
+            let offsetRow =+ rows;
+            for(let k = 0; k < columns; k++) {
+                let offsetColumn =+ columns;
+                if()
+            }
+        }
+    }
 };

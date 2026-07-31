09edadde9bd5acf9247a6d6452eb3dce8c65d3fa
aries510 <darenrespicio@gmail.com>
2026-07-31 10:07:38 -0600

    applying styling

--- client/Aries-Minesweeper/src/App.jsx
+++ client/Aries-Minesweeper/src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Board from './components/Board';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from "./GlobalStyle";
+import { gundamTheme } from './theme';
+
 
 function App() {
     const [gameId, setGameId] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={gundamTheme}>
+      <GlobalStyle />
       <Routes>
         <Route path='/' element={<Board key={ gameId } restart={() => setGameId(gameId + 1)}/>}/>
         {/* <Route path='/win' element={<WinScreen />}/> */}
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 

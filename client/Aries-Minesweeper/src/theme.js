13f05aa22bc3df0f6c840ffdcedf461d01a75a24
aries510 <darenrespicio@gmail.com>
2026-07-31 10:02:13 -0600

    Add gundamTheme with color definitions

---
+++ client/Aries-Minesweeper/src/theme.js
@@ -0,0 +1,22 @@
+export const gundamTheme = {
+
+  primaryBlue: "#0A4EC2",
+  primaryRed: "#C20A0A",
+  primaryYellow: "#F2D024",
+
+ 
+  whiteArmor: "#F5F5F5",
+  metalGray: "#A9A9A9",
+  darkFrame: "#3A3A3A",
+
+
+  warningOrange: "#FF8A00",
+  psychoGreen: "#00FF9C",
+
+
+  uiText: "#E2E2E2",
+  panelLine: "#2B2B2B",
+
+
+  beamGlow: "0 0 10px #FF3B3B",
+};
